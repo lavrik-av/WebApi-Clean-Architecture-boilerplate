0948f870f3041d652e0fada0f2634e2f90e6ceff
Alex Lavrik <alex.l@integrio.net>
2024-01-26 17:17:50 +0200

    Make changes in the ConfigurePersistence method - Add DOCKER_COMPOSE_MODE && DOCKER_MODE environment variables

--- Boilerplate.Persistence/DependencyInjection.cs
+++ Boilerplate.Persistence/DependencyInjection.cs
@@ -10,19 +10,25 @@ namespace Boilerplate.Persistence
         {
             services.AddDbContext<ApplicationDbContext>(options =>
             {
-                //local MS SQL Server
-                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
 
+                var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+                if (Environment.GetEnvironmentVariable("DOCKER_COMPOSE_MODE") is not null && 
+                    Environment.GetEnvironmentVariable("DOCKER_COMPOSE_MODE") == "Y")
+                {
+                    connectionString = configuration.GetConnectionString("DefaultConnectionDocker");
+                }
+                else if (Environment.GetEnvironmentVariable("DOCKER_MODE") is not null &&
+                    Environment.GetEnvironmentVariable("DOCKER_MODE") == "Y")
+                {
+                    throw new NotImplementedException("The app should be run with Docker Compose only as it uses other relaited services within a Docker container");
+                }
+
+                options.UseSqlServer(connectionString);
+
+                Console.WriteLine($"============== SQL Server Connection string ===================");
+                Console.WriteLine($"=============== {connectionString} ==================");
                 Console.WriteLine($"=================================");
-                Console.WriteLine($"=============== {configuration.GetConnectionString("DefaultConnection")} ==================");
-                Console.WriteLine($"=================================");
-
-
-                // local MySql server                
-                // options.UseMySQL(configuration.GetConnectionString("MySqlConnection"));
-
-                // Azure MS SQL Server
-                // options.UseSqlServer(configuration.GetConnectionString("DefaultConnectionAzure"));
             });
 
             services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
--- Boilerplate.UnitTests/Systems/Controllers/TestEntitiesController.cs
+++ Boilerplate.UnitTests/Systems/Controllers/TestEntitiesController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Boilerplate.Application.Interfaces;
+using Boilerplate.WebApi.Controllers;
+using MediatR;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -7,7 +11,21 @@ namespace Boilerplate.UnitTests.Systems.Controllers
     public class TestEntitiesController
     {
         [Fact]
-        internal void GetEntities_onSuccess()
-        { }
+        internal void GetEntities_OnSuccess()
+        {
+            //Arrange
+            // set up the needed dependency
+            // ISender sender, IGenericTypeMediator genericTypeMediator
+
+            var sender = new Mock<ISender>();
+            var generigTypeMediator = new Mock<IGenericTypeMediator>();
+
+            var sut = new EntitiesController(sender.Object, generigTypeMediator.Object);
+
+            //Act
+            sut.GetEntities();
+
+            //Assert
+        }
     }
 }
